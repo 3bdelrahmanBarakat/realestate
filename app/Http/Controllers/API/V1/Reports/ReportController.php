487b9db819e223497add13abb58c042cbf8a6219
Abdelrahman Barakat <abdelrahmanbarakat16@gmail.com>
2024-07-25 19:24:25 +0300

    chore: Remove unused code in ReportController

--- app/Http/Controllers/API/V1/Reports/ReportController.php
+++ app/Http/Controllers/API/V1/Reports/ReportController.php
@@ -13,7 +13,6 @@ use App\Interfaces\ReportInterface;
 
 class ReportController extends Controller
 {
-    //h
     use APIResponse;
 
     public function __construct(public ReportInterface $ReportInterface)
